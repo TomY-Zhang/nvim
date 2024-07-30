304467b7fd724704b97bad624d3f7ec054ce3c85
Tom Zhang <tom.zhang@nyu.edu>
2024-07-30 10:52:02 -0700

    initial config

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.tabstop = 5
+vim.o.shiftwidth = 5
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.cursorline = true
+vim.o.wrap = true
